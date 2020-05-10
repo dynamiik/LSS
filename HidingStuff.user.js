3aba50c29081f36a577448c87cdc116393095f83
dynamiik <31204540+dynamiik@users.noreply.github.com>
2020-05-10 21:51:46 +0200

    Create HidingStuff.user.js

---
+++ HidingStuff.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Hiding Verband
+// @version      1
+// @description  Sachen ausblenden
+// @author       Dynamiite
+// @include      *://www.leitstellenspiel.de/missions/*
+// @grant        none
+// @namespace
+// ==/UserScript==
+/* global $ */
+(function() {
+    var sendVerband=false;
+    f_sendVerband();
+    let button_class = (sendVerband) ? "btn-success" : "btn-danger";
+        //Fügt Buttons in neue DIV. onclick toggelt Boolean
+        $('<a href="#" class="btn '+button_class+' btn-sm navbar-btn" title="statusVehicle" style=""><img alt="Hide_ffffff" class="navbar-icon" src="https://www.leitstellenspiel.de/images/alliance.svg" title="Hide"></a>').prependTo($('.navbar-header'))
+            .click(function(e){
+            sendVerband = !sendVerband;
+            f_sendVerband();
+            // Buttonfarbe wie Boolean. So oder so
+            $(this).hasClass("btn-success") ? $(this).removeClass("btn-success").addClass("btn-danger"): $(this).addClass("btn-success").removeClass("btn-danger");
+            return false;
+        });
+    function f_sendVerband(){
+        if(!sendVerband){
+            $('.alert_next_alliance').hide();
+            $('.navbar-header').children().last().hide();
+            $('form#new_mission_reply').hide()
+        }else{
+            $('.alert_next_alliance').show();
+            $('.navbar-header').children().last().show();
+            $('form#new_mission_reply').show()
+        }
+    }
+})();
