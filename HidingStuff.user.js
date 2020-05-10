09909b0b168602e8a7fcacfe49b1b02ba0129b7f
dynamiik <31204540+dynamiik@users.noreply.github.com>
2020-05-10 22:32:42 +0200

    Update HidingStuff.user.js

--- HidingStuff.user.js
+++ HidingStuff.user.js
@@ -6,11 +6,11 @@
 // @include      *://leitstellenspiel.de/
 // @include      *://www.leitstellenspiel.de/
 // @grant        none
+// @namespace
 // ==/UserScript==
 /* global $ */
 (function() {
     'use strict';
-
     /* Configuration */
     var statusVehicle = false;
     var myVehicle = false;
@@ -18,9 +18,10 @@
     var myBuilding = false
     var verbandVehicle = true;
     var verbandMission = true;
-    var filter = ['[Verband]', '[Event]'];
+    var filter = ['[Verband]', '[Event]']; // Filter für Fahrzeug und Gebäude und Missionen text
     var substringCount = filter.length;
 
+    // ruft setIntervall alle 10 sekunden auf
     var tid = setInterval(check_intervall, 10000);
     init();
     check_intervall();
@@ -36,73 +37,75 @@
         $('<li id="dropdown_hiding_settings_7"></li>').appendTo('#dropdown_hiding_settings .dropdown-menu');
 
         let button_class = (myVehicle) ? "ausgeblendet":"angezeigt";
-        //Fügt Buttons in neue DIV. onclick toggelt Boolean
+        //Fügt Buttons in neue li. onclick toggelt Boolean und ruft function auf
         $('<a href="#" title="myVehicle">Eigene Fahrzeuge <span>'+button_class+'</span></a>').appendTo($('#dropdown_hiding_settings_1'))
             .click(function(e){
             myVehicle = !myVehicle;
             f_myVehicle();
-            // Buttonfarbe wie Boolean. So oder so
+            // Buttontext wechseln
             $(this).find('span').text() == "angezeigt" ?  $(this).find('span').text("ausgeblendet"):$(this).find('span').text("angezeigt");
             return false;
         });
         button_class = (myMission) ? "ausgeblendet":"angezeigt";
-        //Fügt Buttons in neue DIV. onclick toggelt Boolean
+        //Fügt Buttons in neue li. onclick toggelt Boolean und ruft function auf
         $('<a href="#" title="myVehicle">Eigene Missionen <span>'+button_class+'</span></a>').appendTo($('#dropdown_hiding_settings_2'))
             .click(function(e){
             myMission = !myMission;
             f_myMission();
-            // Buttonfarbe wie Boolean. So oder so
+            // Buttontext wechseln
             $(this).find('span').text() == "angezeigt" ?  $(this).find('span').text("ausgeblendet"):$(this).find('span').text("angezeigt");
             return false;
         });
         button_class = (myBuilding) ? "ausgeblendet":"angezeigt";
-        //Fügt Buttons in neue DIV. onclick toggelt Boolean
+        //Fügt Buttons in neue li. onclick toggelt Boolean und ruft function auf
         $('<a href="#" title="myBuilding">Eigene Gebäude <span>'+button_class+'</span></a>').appendTo($('#dropdown_hiding_settings_3'))
             .click(function(e){
             myBuilding = !myBuilding;
             f_myBuilding();
-            // Buttonfarbe wie Boolean. So oder so
+            // Buttontext wechseln
             $(this).find('span').text() == "angezeigt" ?  $(this).find('span').text("ausgeblendet"):$(this).find('span').text("angezeigt");
             return false;
         });
         button_class = (verbandVehicle) ? "ausgeblendet":"angezeigt";
-        //Fügt Buttons in neue DIV. onclick toggelt Boolean
+        //Fügt Buttons in neue li. onclick toggelt Boolean und ruft function auf
         $('<a href="#" title="verbandVehicle">Verband Fahrzeuge <span>'+button_class+'</span></a>').appendTo($('#dropdown_hiding_settings_4'))
             .click(function(e){
             verbandVehicle = !verbandVehicle;
             f_verbandVehicle();
-            // Buttonfarbe wie Boolean. So oder so
+            // Buttontext wechseln
             $(this).find('span').text() == "angezeigt" ?  $(this).find('span').text("ausgeblendet"):$(this).find('span').text("angezeigt");
             return false;
         });
         button_class = (verbandMission) ? "ausgeblendet":"angezeigt";
-        //Fügt Buttons in neue DIV. onclick toggelt Boolean
+        //Fügt Buttons in neue li. onclick toggelt Boolean und ruft function auf
         $('<a href="#" title="verbandMission">Verbands Missionen <span>'+button_class+'</span></a>').appendTo($('#dropdown_hiding_settings_5'))
             .click(function(e){
             verbandMission = !verbandMission;
             f_verbandMission();
-            // Buttonfarbe wie Boolean. So oder so
+            // Buttontext wechseln
             $(this).find('span').text() == "angezeigt" ?  $(this).find('span').text("ausgeblendet"):$(this).find('span').text("angezeigt");
             return false;
-        });
+        });        
         button_class = (statusVehicle) ?  "ausgeblendet":"angezeigt";
-        //Fügt Buttons in neue DIV. onclick toggelt Boolean
+        //Fügt Buttons in neue li. onclick toggelt Boolean und ruft function auf
         $('<a href="#"  title="statusVehicle">Freie Fahrzeuge <span>'+button_class+'</span></a>').appendTo($('#dropdown_hiding_settings_7'))
             .click(function(e){
             statusVehicle = !statusVehicle;
             f_statusVehicle();
-            // Buttonfarbe wie Boolean. So oder so
+            // Buttontext wechseln
             $(this).find('span').text() == "angezeigt" ?  $(this).find('span').text("ausgeblendet"):$(this).find('span').text("angezeigt");
             return false;
         });
     }
     function check_intervall() {
+// wird alle 10 sekunden aufgerufen um alles auszublenden was durch die buttons gesetzt wurde
         if(myBuilding)f_myBuilding();
         if(myVehicle)f_myVehicle();
         if(verbandMission)f_verbandMission();
         if(verbandVehicle)f_verbandVehicle();
         if(statusVehicle)f_statusVehicle();
     }
+    //Blendet die Fahrzeuge aus in der Fahrzeugliste bei status 3 4 6
     function f_statusVehicle(){
         if(statusVehicle){
             let data = $('#building_list span').map(function() {
@@ -112,12 +115,14 @@
             }).get();
         }
         else{
+            // blendet sie wieder ein
             let data = $('#building_list span').map(function() {
                 $(this).parent().show();
                 return;
             }).get();
         }
     }
+    // blendet eigene Fahrzeuge aus
     function f_myVehicle(){
         if(myVehicle){
             for (let i = vehicle_markers.length - 1; i >= 0; i--) {
@@ -128,6 +133,7 @@
             }
         }
         else{
+            // fügt sie wieder ein
             for (let i = vehicle_markers.length - 1; i >= 0; i--) {
                 let string = vehicle_markers[i]._tooltip._content;
                 if (!filter.some(substring => string.includes(substring))) {
@@ -136,6 +142,7 @@
             }
         }
     }
+    // blendet Verbansfahrzeuge aus
     function f_verbandVehicle(){
         if(verbandVehicle){
             for (let i = vehicle_markers.length - 1; i >= 0; i--) {
@@ -146,6 +153,7 @@
             }
         }
         else{
+            // fügt sie wieder ein
             for (let i = vehicle_markers.length - 1; i >= 0; i--) {
                 let string = vehicle_markers[i]._tooltip._content;
                 if (filter.some(substring => string.includes(substring))) {
@@ -154,6 +162,7 @@
             }
         }
     }
+    //blendet eigene Missionen aus
     function f_myMission(){
         if(myMission){
             for (let i = mission_markers.length - 1; i >= 0; i--) {
@@ -163,6 +172,7 @@
                 }
             }
         }
+        // fügt sie wieder ein
         else{
             for (let i = mission_markers.length - 1; i >= 0; i--) {
                 let string = mission_markers[i]._tooltip._content;
@@ -172,6 +182,7 @@
             }
         }
     }
+    // blendet Verbandsmissionen aus
     function f_verbandMission(){
         if(verbandMission){
             for (let i = mission_markers.length - 1; i >= 0; i--) {
@@ -181,6 +192,7 @@
                 }
             }
         }
+        // fügt sie wieder ein
         else{
             for (let i = mission_markers.length - 1; i >= 0; i--) {
                 let string = mission_markers[i]._tooltip._content;
@@ -190,6 +202,7 @@
             }
         }
     }
+    // blendet eigene Gebäude aus und fügt sie wieder ein
     function f_myBuilding(){
         if(myBuilding) $(".leaflet-interactive[src*='building']").hide();
         else $(".leaflet-interactive[src*='building']").show();
