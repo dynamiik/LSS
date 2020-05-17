9e7fb51168130fad59f87516b9d272a85dcc94a8
dynamiik <31204540+dynamiik@users.noreply.github.com>
2020-05-17 23:59:35 +0200

    Create FahrzeugSort.user.js

---
+++ FahrzeugSort.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Einsatzfenster Fahrzeug sort
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Sortiert die angeklickten Fahrzeuge in dem Einsatzfenster
+// @author       Dynamiite
+// @include      *://www.leitstellenspiel.de/missions*
+// @include      *://www.leitstellenspiel.de/missions*
+// @include      *://www.missionchief.co.uk/missions*
+// @include      *://www.missionchief.com/missions*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setTimeout( ()=>{
+        let clicky = $('<div>^</div>')
+        $(clicky).prependTo($('#vehicle_show_table_all .tablesorter-headerRow').children()[1])
+        $(clicky).click(()=>bringUPtheVehicles())
+        function bringUPtheVehicles(){
+            var checked = $('.vehicle_checkbox:checked');
+            for(let i=checked.length-1;i>=0;i--){
+                $('#vehicle_element_content_'+$(checked[i])[0].value).prependTo($('#vehicle_show_table_body_all'))
+            }
+        }
+    },500)
+})();
