b367f1db8956dd63114f5b83fa64311ebdaec8ec
dynamiik <31204540+dynamiik@users.noreply.github.com>
2020-05-31 18:48:51 +0200

    Update testSkript.user.js

--- testSkript.user.js
+++ testSkript.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Graphicsets
+// @name         Graphicsets Test
 // @namespace    http://tampermonkey.net/
 // @version      0.0.1
 // @description  try to take over the world!
